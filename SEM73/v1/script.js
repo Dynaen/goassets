f4aa7a36f3fa97e7a38db8135fa9ef33599e65c1
Dynaen <dyna@pandia.dk>
2023-11-16 11:47:49 +0100

    Add files via upload

---
+++ SEM73/v1/script.js
@@ -0,0 +1,40 @@
+function checkIfReady(fn) {
+  if (document.readyState != "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+checkIfReady(function () {
+  createWidget();
+});
+
+function createWidget() {
+  let uri = window.location.href;
+  let currentURL = encodeURIComponent(uri);
+  const article = document.querySelector(
+    "div[data-test='template-article-base'] div[data-parse] + div"
+  );
+
+  const widget = document.createElement("div");
+  widget.id = "sp-widget";
+  widget.innerHTML = `<h6>Page Stats for [month]</h6>
+    <p class="sp-strapline">Data from Sermrush tools</p>
+    <div class="sp-cols">
+    <div class="sp-col"><p>Traffic</p><p class="sp-numbers">XXX</p></div>
+    <div class="sp-col"><p>Keywords</p><p class="sp-numbers">XXX</p></div>
+    <div class="sp-col"><p>Backlinks</p><p class="sp-numbers">XXX</p></div>
+    </div>
+    <a href="https://www.semrush.com/analytics/overview/?searchType=subfolder&q=${currentURL}" id="sp-cta"><span data-test="text-content">Try for Free →</span></a>
+    <p class="sp-cta-sub">Get these stats and more for any page!</p>`;
+  if (article) {
+    article.prepend(widget);
+    let widgetHeight = document.querySelector("#sp-widget").clientHeight;
+    let otherWidget = document.querySelector(
+      "section[data-test='domain-overview-widget']"
+    );
+    if (otherWidget) {
+      otherWidget.style.top = `${widgetHeight + 136 + 16}px`;
+    }
+  }
+}
